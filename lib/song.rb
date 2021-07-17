8cf9eacc733b4866660a50e16ddac2ce3b119a2b
dara <just4oludee@yahoo.com>
2021-07-17 19:56:04 -0400

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
+class Song
+    attr_reader :artist, :genre
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artsit = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+end
